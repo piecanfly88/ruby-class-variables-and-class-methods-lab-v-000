ea17ea14581bd79419074172da131237477c613f
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-26 06:38:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,5 +35,18 @@ class Song
       else
         genre_count[genre] = 1
       end
+      genre_count
     end
+
+    def self.artist_count
+      artist_count = {}
+      @@artists.each do |artist|
+        if artist_count[artist]
+          artist_count[artist] += 1
+        else
+          artist_count[artist] = 1
+        end
+        artist_count
+      end
+    end  
 end
